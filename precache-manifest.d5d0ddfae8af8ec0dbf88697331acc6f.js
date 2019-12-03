fa8ccf414f104f4ea9ad1eae990d564abd82e92a
Miroslav Beno <miroxbeno@gmail.com>
2019-12-03 20:28:35 +0100

    Updates

---
+++ precache-manifest.d5d0ddfae8af8ec0dbf88697331acc6f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd5144557c2725098a980be78fabb084",
+    "url": "/hqstock-app/static/media/logoText.fd514455.svg"
+  },
+  {
+    "revision": "035eede1115f65333c16",
+    "url": "/hqstock-app/static/css/main.08b3d9e8.chunk.css"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/hqstock-app/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "29ffd6382b0812d265a9",
+    "url": "/hqstock-app/static/js/1.29ffd638.chunk.js"
+  },
+  {
+    "revision": "3cd4fce4fd5d57eb9738",
+    "url": "/hqstock-app/static/js/runtime~main.3cd4fce4.js"
+  },
+  {
+    "revision": "d9f01863e52ea87f73f3a5e6255f6a85",
+    "url": "/hqstock-app/static/media/logo.d9f01863.svg"
+  },
+  {
+    "revision": "035eede1115f65333c16",
+    "url": "/hqstock-app/static/js/main.035eede1.chunk.js"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/hqstock-app/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/hqstock-app/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/hqstock-app/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "29ffd6382b0812d265a9",
+    "url": "/hqstock-app/static/css/1.651a699b.chunk.css"
+  },
+  {
+    "revision": "9641b909fbc77f63149b987b6818aa5a",
+    "url": "/hqstock-app/index.html"
+  }
+];
